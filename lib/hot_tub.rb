d0306b1c553dbbd8f014f9edeafee822bf052ef1
Joshua Mckinney <joshmckin@gmail.com>
2012-11-08 08:58:02 -0600

    do not require excon

--- lib/hot_tub.rb
+++ lib/hot_tub.rb
@@ -5,7 +5,7 @@ require "hot_tub/version"
 require "hot_tub/session"
 require "hot_tub/clients/client"
 require "hot_tub/clients/em_synchrony_client"
-require "hot_tub/clients/excon_client"
+#require "hot_tub/clients/excon_client"
 require "hot_tub/clients/http_client_client" if RUBY_VERSION < '1.9' or (defined? RUBY_ENGINE and 'jruby' == RUBY_ENGINE)
 
 module HotTub
