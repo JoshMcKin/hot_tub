83e0d81b4533fd04f4f51454da38ecf07b7da08e
Joshua Mckinney <joshmckin@gmail.com>
2014-03-06 20:14:05 -0600

    document Sessions options

--- lib/hot_tub/sessions.rb
+++ lib/hot_tub/sessions.rb
@@ -39,6 +39,28 @@ module HotTub
     #   sessions.run("https://wwww.google.com") do |conn|
     #     p conn.head.status
     #   end
+    #
+    #
+    # === OPTIONS
+    # [:with_pool]
+    #   Default is false. A boolean that if true, wraps the new_client block in HotTub::Pool.new. All options
+    #   are passed to the new HotTub::Pool object. See HotTub::Pool for options
+    # [:close]
+    #   Default is nil. Can be a symbol representing an method to call on a client to close the client or a lambda
+    #   that accepts the client as a parameter that will close a client. The close option is performed on clients
+    #   on reaping and shutdown after the client has been removed from the pool.  When nil, as is the default, no
+    #   action is performed.
+    # [:clean]
+    #   Default is nil. Can be a symbol representing an method to call on a client to clean the client or a lambda
+    #   that accepts the client as a parameter that will clean a client. When nil, as is the default, no action is
+    #   performed.
+    # [:reap]
+    #   Default is nil. Can be a symbol representing an method to call on a client that returns a boolean marking
+    #   a client for reaping, or a lambda that accepts the client as a parameter that returns a boolean boolean
+    #   marking a  client for reaping. When nil, as is the default, no action is performed.
+    # [:no_reaper]
+    #   Default is nil. A boolean like value that if true prevents the reaper from initializing
+    #
     def initialize(opts={},&new_client)
       raise ArgumentError, "HotTub::Sessions require a block on initialization that accepts a single argument" unless block_given?
       @with_pool        = opts[:with_pool]        # Set to true to use HotTub::Pool with supplied new_client block
