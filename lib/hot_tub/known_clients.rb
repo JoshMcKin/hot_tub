f1dbb879fd73a867a1ad8895c1cb98ece0542eb8
Joshua Mckinney <joshmckin@gmail.com>
2015-11-05 13:45:48 -0600

    dry up KnownClients

--- lib/hot_tub/known_clients.rb
+++ lib/hot_tub/known_clients.rb
@@ -20,11 +20,7 @@ module HotTub
     def clean_client(clnt)
       if @clean_client
         begin
-          if @clean_client.is_a?(Proc)
-            preform_client_block(clnt,&@clean_client)
-          else
-            preform_client_method(clnt,@clean_client)
-          end
+          perform_action(clnt,@clean_client)
         rescue => e
           HotTub.logger.error "[HotTub] There was an error cleaning one of your #{self.class.name} clients: #{e}" if HotTub.logger
         end
@@ -38,11 +34,7 @@ module HotTub
       @close_action = (@close_client || known_client_action(clnt,:close) || false) if @close_action.nil?
       if @close_action
         begin
-          if @close_action.is_a?(Proc)
-            preform_client_block(clnt,&@close_action)
-          else
-            preform_client_method(clnt,@close_action)
-          end
+          perform_action(clnt,@close_action)
         rescue => e
           HotTub.logger.error "[HotTub] There was an error closing one of your #{self.class.name} clients: #{e}" if HotTub.logger
         end
@@ -55,11 +47,7 @@ module HotTub
       rc = false
       if @reap_client
         begin
-          if @reap_client.is_a?(Proc)
-            rc = preform_client_block(clnt,&@reap_client)
-          else
-            rc = preform_client_method(clnt,@reap_client)
-          end
+          rc = perform_action(clnt,@reap_client)
         rescue => e
           HotTub.logger.error "[HotTub] There was an error reaping one of your #{self.class.name} clients: #{e}" if HotTub.logger
         end
@@ -73,12 +61,18 @@ module HotTub
       (KNOWN_CLIENTS[clnt.class.name] && KNOWN_CLIENTS[clnt.class.name][key])
     end
 
-    def preform_client_block(clnt,action=nil)
-      yield clnt
+    def perform_action(clnt,action)
+      if action.is_a?(Proc)
+        yield_action(clnt,&action)
+      else
+        clnt.__send__(action)
+      end
     end
 
-    def preform_client_method(clnt,action=nil)
-      clnt.send(action)
+    # This is ever so slightly faster in MRI,
+    #  more so on Rubinius and Jruby
+    def yield_action(clnt)
+      yield clnt
     end
   end
 end
